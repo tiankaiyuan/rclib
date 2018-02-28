631d29f62863cd3d13a9991183545bb90b4fb6cd
tiankaiyuan <tky2030@163.com>
2018-02-28 14:04:04 +0800

    加入样式

--- shared/containers/Root.js
+++ shared/containers/Root.js
@@ -2,8 +2,10 @@
  * Created by tiankaiyuan on 2018/2/27.
  */
 import React from 'react'
+//使用babel-plugin-transform-require-ignore 这个插件后 被忽略的文件类型，不能以变量的形式引入，这种形式是不行的import xx from './root.less'
+import  './root.less';
 export default ()=>{
-    return (<div>
+    return (<div className="root-component">
         hello world
     </div>)
 }
